66b5fdfda7c17e4684df8a34577fc4ffd2fce95e
Lance Easley <atlasle52671015@gmail.com>
2021-05-18 14:16:17 -0500

    Added java as a skill and made subtitle animation

---
+++ index.js
@@ -0,0 +1,13 @@
+const titleSubHeading = document.querySelector("#title-subtext")
+
+function typeAnimation(text, speed) {
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            titleSubHeading.textContent += text[i];
+        }, i * speed)
+    };
+};
+
+setTimeout(() => {
+    typeAnimation("I do code stuff", 100)
+}, 2000)
