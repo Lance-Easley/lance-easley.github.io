7bef486217dee861b6a7d44eb23a07d8ff5c81ba
Lance Easley <atlasle52671015@gmail.com>
2021-05-20 14:39:30 -0500

    Grammar & styling. Added click-to-scroll to arrow

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const titleSubHeading = document.querySelector("#title-subtext");
+const arrowElement = document.querySelector("#arrow");
+const mainSectionElement = document.querySelector("#main-section");
 
 function typeAnimation(text, speed) {
     for (let i = 0; i < text.length; i++) {
@@ -7,6 +9,10 @@ function typeAnimation(text, speed) {
         }, i * speed)
     };
 };
+
+arrowElement.addEventListener("click", () => {
+    mainSectionElement.scrollIntoView(true);
+})
 
 setTimeout(() => {
     typeAnimation("I do code stuff", 100)
